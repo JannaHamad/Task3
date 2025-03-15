843d88caf25d03d7291028b0cde36d838f8d556d
JannaHamad <jannajaser304@gmail.com>
2025-03-15 02:36:36 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,5 @@
+
+//document.getElementById("hello").textContent="Janna"
+document.querySelector(".hello").textContent="Janna Hamad"
+document.querySelector("div p").classList.add("test");
+document.getElementById("Div").classList.add("changebackground"); 
